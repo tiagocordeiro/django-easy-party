8845ef5eba3bf9a36174a51ef5ac8f6a73c026d8
Tiago Cordeiro <tiago@mulhergorila.com>
2020-02-11 14:33:57 -0300

    Testes com static files in views

--- invitation/facade.py
+++ invitation/facade.py
@@ -13,6 +13,7 @@ locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 def make_invite(pk):
     invite = Invite.objects.get(pk=pk)
     # invite.image = Image.open(f'{settings.STATIC_ROOT}/convites/convite.jpg')
+    print(static('convites/convite.jpg'))
     invite.image = Image.open(static('convites/convite.jpg'))
     draw = ImageDraw.Draw(invite.image)
     invite.date_string = invite.date.strftime("%d • %B • %Y")
