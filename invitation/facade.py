d78846a29439e8e04424aef6b15e5c6ed9892e4b
Tiago Cordeiro <tiago@mulhergorila.com>
2020-02-11 18:07:59 -0300

    Testes com static files in views

--- invitation/facade.py
+++ invitation/facade.py
@@ -1,6 +1,7 @@
 import locale
 
 from PIL import Image, ImageDraw, ImageFont
+from django.templatetags.static import static
 from django.utils.text import slugify
 
 from easyparty import settings
@@ -12,6 +13,7 @@ locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
 def make_invite(pk):
     invite = Invite.objects.get(pk=pk)
     print(f'{settings.STATIC_ROOT}/convites/convite.jpg')
+    print(static('convites/convite.jpg'))
     invite.image = Image.open(f'{settings.STATIC_ROOT}/convites/convite.jpg')
     draw = ImageDraw.Draw(invite.image)
     invite.date_string = invite.date.strftime("%d • %B • %Y")
