6e6341e4419f0a44f4548be366da48d864d67693
Tiago Cordeiro <tiago@mulhergorila.com>
2020-02-12 10:46:45 -0300

    Improves documentation
    - Adiciona badges ao README.md
    - Adiciona codecov ao .travis.yml
    
    This close #7

--- invitation/tests.py
+++ invitation/tests.py
@@ -1,6 +1,6 @@
 from datetime import date
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.test import TestCase, RequestFactory, Client
 from django.urls import reverse
 
@@ -16,6 +16,8 @@ class InvitationTestCase(TestCase):
 
         # Staff user
         self.user_staff = User.objects.create_user(username='jacob', email='jacob@…', password='top_secret')
+        self.group = Group.objects.create(name='Testes')
+        self.group.user_set.add(self.user_staff)
 
         self.invite_brian = Invite.objects.create(name='Brian', age=12, gender=1, date=date.today(), start_time='18:00',
                                                   end_time='21:00', maximum_guests=33)
